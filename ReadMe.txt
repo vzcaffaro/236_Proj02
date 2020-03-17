ca30e44dd806600db8851f51b65eea0f947180f0
vzcaffaro <caffaro@chapman.edu>
2020-03-16 18:38:05 -0700

    Initial Commit

---
+++ ReadMe.txt
@@ -0,0 +1,17 @@
+Vinny Caffaro
+002298196
+caffaro@chapman.edu
+CPSC 236
+Project 2
+
+Notes:
+The scoreText object will not stay in place. 
+When starting the game it is not visible and it sometimes jumps around the axes. 
+I have even tried adding a rigidbody to freeze its location but I cannot fix the issue.
+The score does track properly, and can be seen if you hit pause and find it.
+
+Also I could not get the screen to scroll. It seems I am running a different version of unity.
+I was unable to add the nebula_blue to just the plane, instead the entire image is added.
+
+For the extra project component, I added an enemy that shoots 2 bullets at a diagonal.
+The spread of the bullets and the ratio of enemies are both modifiable.
---
+++ vCaffaro_Proj02/Assets/Scripts/Enemy2Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2Controller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    private float timerBullet;
+    private float maxTimerBullet;
+    public GameObject bullet1;
+    public GameObject bullet2;
+
+    public float timerMin = 15f;
+    public float timerMax = 25f;
+    public bool canFireBullets = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+
+        timerBullet = 0;
+        maxTimerBullet = Random.Range(timerMin, timerMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFireBullets)
+            StartCoroutine("FireBullet");
+        if (Camera.main.WorldToViewportPoint(transform.position).y < 0)
+            Destroy(this.gameObject);
+    }
+
+    void SpawnBullet()
+    {
+        Vector3 spawnPoint = transform.position;
+        spawnPoint.y -= (bullet1.GetComponent<Renderer>().bounds.size.y / 2) + (GetComponent<Renderer>().bounds.size.y / 2);
+        GameObject.Instantiate(bullet1, spawnPoint, transform.rotation);
+        GameObject.Instantiate(bullet2, spawnPoint, transform.rotation);
+    }
+
+    IEnumerator FireBullet()
+    {
+        if (timerBullet >= maxTimerBullet)
+        {
+            SpawnBullet();
+            timerBullet = 0;
+            maxTimerBullet = Random.Range(timerMin, timerMax);
+        }
+
+        timerBullet += 0.1f;
+        yield return new WaitForSeconds(0.1f);
+    }
+
+}
